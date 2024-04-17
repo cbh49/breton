548c472698274843b0e8015a29764621d44dcbf0
cbh49 <76746884+cbh49@users.noreply.github.com>
2024-04-17 18:37:58 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    images: {
+      domains: ['cdn.freebiesupply.com', 'seeklogo.com'],
+    },
+    // ... any other config options you have
+  };
+ 
+  export default nextConfig;
+ 
