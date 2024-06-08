5b8034f5b63341bce05058d5859d637c8bd077fe
cbh49 <76746884+cbh49@users.noreply.github.com>
2024-06-08 12:43:39 -0500

    Add files via upload

---
+++ pages/news.tsx
@@ -0,0 +1,194 @@
+// pages/index.tsx
+import styles from '../styles/news.module.css';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState, useRef } from 'react';
+
+// Define the matchup type if you have specific structure for matchup data
+type Matchup = {
+  id: number;
+  team1Logo: string;
+  Team1: string;
+  team2Logo: string;
+  Team2: string;
+  Total: number;
+};
+type AdjMatchData = {
+  Team1: string;
+  Team2: string;
+  Total: number;
+  adj_total: number;
+  difference: number;
+  original_total: number;
+  Team1total: number;
+  Team2total: number;
+  MLdifference: number;
+  Team1Pitcher: string;
+  Team2Pitcher: string;
+};
+
+type LogoUrls = { [team: string]: string };
+
+const Home = () => {
+  const [matchups, setMatchups] = useState<Matchup[]>([]);
+  const [logos, setLogos] = useState<LogoUrls>({});
+  const [isNbaDropdownVisible, setIsNbaDropdownVisible] = useState(false);
+  const [isMLBDropdownVisible, setIsMLBDropdownVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [adjMatchData, setAdjMatchData] = useState<AdjMatchData[]>([]);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+  const [isSubscribed, setIsSubscribed] = useState(true);
+  const [currentMatchupIndex, setCurrentMatchupIndex] = useState(0);
+  const [displayMatchups, setDisplayMatchups] = useState<Matchup[]>([]);
+  const numGames = matchups.length;
+  const tableTopSpacing = `${450}px`;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tableMarginTop, setTableMarginTop] = useState(0);
+
+  useEffect(() => {
+    // Fetch the logos on component mount
+    fetch('/mlblogos.json')
+      .then(response => response.json())
+      .then(data => setLogos(data))
+      .catch(error => console.error('Error fetching logos:', error));
+
+      fetch('/mlbmatchups.json')
+      .then(response => response.json())
+      .then((data: Matchup[]) => {  // Explicitly type 'data' as an array of 'Matchup'
+        setMatchups(data);
+        data.forEach((matchup: Matchup) => {  // Now 'matchup' is also typed as 'Matchup'
+          console.log(matchup.team2Logo);
+        });
+      })
+      .catch(error => console.error('Error fetching matchups:', error));
+      }, []);
+
+    // Rotating Matchups
+    const handleNextGames = () => {
+      setCurrentMatchupIndex(prevIndex => {
+        const newIndex = prevIndex + 7;
+        return newIndex >= matchups.length ? 0 : newIndex; // Resets to 0 if exceeds array length
+      });
+    };
+    useEffect(() => {
+      const newDisplayMatchups = matchups.slice(currentMatchupIndex, currentMatchupIndex + 7);
+      setDisplayMatchups(newDisplayMatchups);
+    }, [currentMatchupIndex, matchups]);
+    
+
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const contentBottom = contentRef.current.getBoundingClientRect().bottom;
+      const navbarBottom = document.querySelector('.navbar')?.getBoundingClientRect().bottom || 0;
+      let newMarginTop;
+
+      if (adjMatchData.length === 0) {
+        // When there's no data, center the table on the screen
+        newMarginTop = (window.innerHeight - contentBottom) / 2;
+      } else {
+        // When there's data, set a smaller margin
+        newMarginTop = 30; // Or any other suitable value based on your design
+      }
+
+      setTableMarginTop(Math.max(newMarginTop, 30)); // Ensures that the margin is not less than 20px
+    }
+  }, [adjMatchData]);
+    return (
+      <>
+        <Head>
+          <title>BRETON</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className={styles.banner}>
+        </div>
+        <div className={styles.navbar}>
+      <Image src="/bretpngw.png" alt="Logo" width={100} height={100} className={styles.logo} />
+      <Link href="/" passHref></Link>
+      <ul>
+      <li><Link href="/">HOME</Link></li>
+        <li
+          onMouseEnter={() => setIsMLBDropdownVisible(true)}
+          onMouseLeave={() => setIsMLBDropdownVisible(false)}
+        >
+          MLB
+          {isMLBDropdownVisible && (
+            <div className={styles.dropdown}>
+              <Link href="/mlb"><p>Over/Under</p></Link>
+              <Link href="/mlbml"><p>ML PICKS</p></Link>
+              <Link href="/nrfi"><p>NRFI</p></Link>
+            </div>
+          )}
+        </li>
+        <li
+          onMouseEnter={() => setIsNbaDropdownVisible(true)}
+          onMouseLeave={() => setIsNbaDropdownVisible(false)}
+        >
+          NBA
+          {isNbaDropdownVisible && (
+            <div className={styles.dropdown}>
+              <Link href="/nba"><p>Over/Under</p></Link>
+              <Link href="/fullprop"><p>Player Props</p></Link>
+              <Link href="/prop"><p>Alt Player Props</p></Link>
+            </div>
+          )}
+        </li>
+        <li><Link href="/news">NEWS</Link></li>
+        <li><Link href="/ncaab">CBB</Link></li>
+      </ul>
+      <div className={styles.odds}>
+      <h4>Odds via:</h4>
+      <Image src="/dkvert.png" alt="Logo" width={70} height={60} className={styles.logor} />
+      </div>
+    </div>
+        <div className={styles.content}>
+        <span className={styles.span}/> 
+          <h1>News</h1>
+          <p>Follow @breton on Twitter. <br /> <br /> <br /> <Link href="https://twitter.com/BretonPicks"><Image src="/x.png" alt="Logo" width={200} height={200} className={styles.bottomnavlogo} /></Link> <br /> <br /> <br /> News Coming Soon.</p>
+        </div>
+        <div className={styles.sideNav} onMouseEnter={() => setIsVisible(true)} onMouseLeave={() => setIsVisible(false)}>
+          {/* Side Navigation content */}
+          <div className={styles.user}>
+            <Image src="/mlb.webp" alt="user-img" width={125} height={80} className={styles.leaguelogo2} />
+          </div>
+          <h3>MLB GAMES TODAY</h3>
+          <table className={styles.table} style={{ top: tableTopSpacing }}>
+        {/* Table content */}
+        <thead>
+          <tr>
+          </tr>
+        </thead>
+              <tbody>
+        {displayMatchups.length === 0 ? (
+          <tr>
+            <td colSpan={4}>No Games Today!</td>
+          </tr>
+        ) : (
+          displayMatchups.map((matchup) => (
+            <tr key={matchup.id} className={styles.matchupRow}>
+                <Image src={logos[matchup.Team1]} alt={matchup.Team1} width={80} height={70} className={styles.navlogo} layout="fixed" />  
+              <td className={styles.atSymbol}>@</td>   
+                <Image src={logos[matchup.Team2]} alt={matchup.Team2} width={80} height={70} className={styles.navlogo} layout="fixed" />
+              <div className={styles.total}>O/U: {matchup.Total}</div>
+            </tr>
+            
+          ))
+        )}
+      </tbody>
+      </table>
+      {isVisible && (
+    <button onClick={handleNextGames} className={styles.nextButton} title="Next Games">
+    <Image
+      src="/right-arrow.png" // Update with your actual image path
+      alt="Next 7 Games"
+      width={50} // Set appropriate width
+      height={50} // Set appropriate height
+    />
+  </button>
+  )}
+          </div>
+      </>
+    );
+  };
+  export default Home;
