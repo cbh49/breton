27b2a298cd111c58e42c47936f34933749161684
cbh49 <76746884+cbh49@users.noreply.github.com>
2024-06-11 17:56:30 -0500

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -316,7 +316,7 @@ const Home = () => {
       </ul>
       <div className={styles.odds}>
       <h4>Odds via:</h4>
-      <Image src="/dkvert.png" alt="Logo" width={70} height={60} className={styles.logor}/>
+      <Image src="/dkvert.png" alt="Logo" width={75} height={65} className={styles.logor}/>
       </div>
     </div>
     
--- pages/prop.tsx
+++ pages/prop.tsx
@@ -1,5 +1,5 @@
 // pages/nba.tsx
-import styles from '../styles/nhl.module.css'; // Ensure you have Ncaab.module.css with appropriate styles
+import styles from '../styles/nba.module.css'; // Ensure you have Ncaab.module.css with appropriate styles
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
