99110de9e0634a4fb88c0072a8df83db61540edb
cbh49 <76746884+cbh49@users.noreply.github.com>
2024-06-13 14:27:35 -0500

    Add files via upload

--- pages/nrfi.tsx
+++ pages/nrfi.tsx
@@ -1,9 +1,8 @@
-// pages/mlb.tsx
-import styles from '../styles/nrfi.module.css'; // Ensure you have Ncaab.module.css with appropriate styles
+import { useEffect, useState, useRef } from 'react';
+import styles from '../styles/nrfi.module.css'; 
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState, useRef } from 'react';
 import React, { Fragment } from 'react';
 
 type LogoUrls = { [team: string]: string };
@@ -19,130 +18,131 @@ type Matchup = {
 };
 
 type AdjMatchData = {
-    id: number;
-    team1Logo: string;
-    Team1: string;
-    team2Logo: string;
-    Team2: string;
-    Team1Pitcher: string;
-    Team1PitcherWHIP: number;
-    Team1Runs: number;
-    Team2Pitcher: string;
-    Team2PitcherWHIP: number;
-    Team2Runs: number;
-    Total: number;
-    PRA1: number;
-    PRA2: number;
+  id: number;
+  team1Logo: string;
+  Team1: string;
+  team2Logo: string;
+  Team2: string;
+  Team1Pitcher: string;
+  Team1PitcherWHIP: number;
+  Team1Runs: number;
+  Team2Pitcher: string;
+  Team2PitcherWHIP: number;
+  Team2Runs: number;
+  Total: number;
+  PRA1: number;
+  PRA2: number;
 };
 
 type TallyData = {
-    "2+": string,
-    "1_to_2": string,
-    "-1_to_-2": string,
-    "-2-": string
+  "2+": string,
+  "1_to_2": string,
+  "-1_to_-2": string,
+  "-2-": string
 };
 
 const MLB = () => {
-    const [adjMatchData, setAdjMatchData] = useState<AdjMatchData[]>([]);
-    // State for the logos and matchups for the sideNav
-    const [logos, setLogos] = useState<LogoUrls>({});
-    const [isSubscribed, setIsSubscribed] = useState(true);
-    const [matchups, setMatchups] = useState<Matchup[]>([]);
-    const [currentMatchupIndex, setCurrentMatchupIndex] = useState(0);
-    const [displayMatchups, setDisplayMatchups] = useState<Matchup[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const numGames = matchups.length;
-    const [isVisible, setIsVisible] = useState(false);
-    const tableTopSpacing = `${450}px`;
-    const [tableMarginTop, setTableMarginTop] = useState(0);
-    const contentRef = useRef<HTMLParagraphElement>(null);
-    const [isNbaDropdownVisible, setIsNbaDropdownVisible] = useState(false);
-    const [isMLBDropdownVisible, setIsMLBDropdownVisible] = useState(false);
-    const [IsChartDropdownVisible, setIsChartDropdownVisible] = useState(false);
-    const [tallies, setTallies] = useState<TallyData>({
-        "2+": "Loading...",
-        "1_to_2": "Loading...",
-        "-1_to_-2": "Loading...",
-        "-2-": "Loading..."
-    });
+  const [adjMatchData, setAdjMatchData] = useState<AdjMatchData[]>([]);
+  const [logos, setLogos] = useState<LogoUrls>({});
+  const [isSubscribed, setIsSubscribed] = useState(true);
+  const [matchups, setMatchups] = useState<Matchup[]>([]);
+  const [currentMatchupIndex, setCurrentMatchupIndex] = useState(0);
+  const [displayMatchups, setDisplayMatchups] = useState<Matchup[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [nrfiResults, setNrfiResults] = useState<{ NRFI: { correct: number, total: number }, YRFI: { correct: number, total: number } } | null>(null);
+  const numGames = matchups.length;
+  const [isVisible, setIsVisible] = useState(false);
+  const tableTopSpacing = `${450}px`;
+  const [tableMarginTop, setTableMarginTop] = useState(0);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+  const [isNbaDropdownVisible, setIsNbaDropdownVisible] = useState(false);
+  const [isMLBDropdownVisible, setIsMLBDropdownVisible] = useState(false);
+  const [IsChartDropdownVisible, setIsChartDropdownVisible] = useState(false);
+  const [tallies, setTallies] = useState<TallyData>({
+    "2+": "Loading...",
+    "1_to_2": "Loading...",
+    "-1_to_-2": "Loading...",
+    "-2-": "Loading..."
+  });
+
   useEffect(() => {
-// Fetch the logos and matchups
-Promise.all([
-    fetch('/mlblogos.json').then(res => res.json()),
-    fetch('/mlbmatchups.json').then(res => res.json()),
-    fetch('/sorted_NRFI.json').then(res => res.json()),
-    fetch('/cumulative_tallies.json').then(res => res.json())
-])
-.then(([logosData, matchupsData, adjMatchOrderData, tallyData]) => { // <- Include tallyData here
-    setLogos(logosData);
-    setMatchups(matchupsData);
-    setAdjMatchData(adjMatchOrderData);
-    setTallies(tallyData); // <- Now tallyData is correctly passed in 
-})
-.catch(error => {
-    console.error('Error fetching data:', error);
-    // If there's an error fetching tallies, you may want to set a default message or value
-    setTallies({
+    // Fetch the logos and matchups
+    Promise.all([
+      fetch('/mlblogos.json').then(res => res.json()),
+      fetch('/mlbmatchups.json').then(res => res.json()),
+      fetch('/sorted_NRFI.json').then(res => res.json()),
+      fetch('/cumulative_tallies.json').then(res => res.json()),
+      fetch('/nrfiresults.json').then(res => res.json()) // Fetch the NRFI results
+    ])
+    .then(([logosData, matchupsData, adjMatchOrderData, tallyData, nrfiResultsData]) => {
+      setLogos(logosData);
+      setMatchups(matchupsData);
+      setAdjMatchData(adjMatchOrderData);
+      setTallies(tallyData);
+      setNrfiResults(nrfiResultsData); // Update the NRFI results state
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      // If there's an error fetching tallies, you may want to set a default message or value
+      setTallies({
         "2+": "Error",
         "1_to_2": "Error",
         "-1_to_-2": "Error",
         "-2-": "Error"
-    });
-})
-.finally(() => {
-    setIsLoading(false);
-});
-
-    }, []);
-
-
-    // Rotating Matchups
-    const handleNextGames = () => {
-      setCurrentMatchupIndex(prevIndex => {
-        const newIndex = prevIndex + 7;
-        return newIndex >= matchups.length ? 0 : newIndex; // Resets to 0 if exceeds array length
       });
-    };
-    useEffect(() => {
-      const newDisplayMatchups = matchups.slice(currentMatchupIndex, currentMatchupIndex + 7);
-      setDisplayMatchups(newDisplayMatchups);
-    }, [currentMatchupIndex, matchups]);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+  }, []);
 
-    // Table Spacing from Text    
-    useEffect(() => {
-      if (contentRef.current) {
-        const contentBottom = contentRef.current.getBoundingClientRect().bottom;
-        const navbarBottom = document.querySelector('.navbar')?.getBoundingClientRect().bottom || 0;
-        let newMarginTop;
-    
-        if (adjMatchData.length === 0) {
-          // When there's no data, center the table on the screen
-          newMarginTop = (window.innerHeight - contentBottom) / 2;
-        } else {
-          // When there's data, set a smaller margin
-          newMarginTop = 20; // Or any other suitable value based on your design
-        }
-    
-        setTableMarginTop(Math.max(newMarginTop, 20)); // Ensures that the margin is not less than 20px
-      }
-    }, [adjMatchData]);
+  // Rotating Matchups
+  const handleNextGames = () => {
+    setCurrentMatchupIndex(prevIndex => {
+      const newIndex = prevIndex + 7;
+      return newIndex >= matchups.length ? 0 : newIndex; // Resets to 0 if exceeds array length
+    });
+  };
 
-    function toFixed(value: number | undefined, decimals: number = 2): string {
-      // Check if the value is a number and is not null or undefined.
-        if (typeof value === 'number') {
-            return value.toFixed(decimals);
-        } else {
-            // Return 'N/A' if the value is not a number.
-            return 'N/A';
+  useEffect(() => {
+    const newDisplayMatchups = matchups.slice(currentMatchupIndex, currentMatchupIndex + 7);
+    setDisplayMatchups(newDisplayMatchups);
+  }, [currentMatchupIndex, matchups]);
+
+  // Table Spacing from Text    
+  useEffect(() => {
+    if (contentRef.current) {
+      const contentBottom = contentRef.current.getBoundingClientRect().bottom;
+      const navbarBottom = document.querySelector('.navbar')?.getBoundingClientRect().bottom || 0;
+      let newMarginTop;
+
+      if (adjMatchData.length === 0) {
+        // When there's no data, center the table on the screen
+        newMarginTop = (window.innerHeight - contentBottom) / 2;
+      } else {
+        // When there's data, set a smaller margin
+        newMarginTop = 20; // Or any other suitable value based on your design
       }
+
+      setTableMarginTop(Math.max(newMarginTop, 20)); // Ensures that the margin is not less than 20px
     }
+  }, [adjMatchData]);
+
+  function toFixed(value: number | undefined, decimals: number = 2): string {
+    // Check if the value is a number and is not null or undefined.
+    if (typeof value === 'number') {
+      return value.toFixed(decimals);
+    } else {
+      // Return 'N/A' if the value is not a number.
+      return 'N/A';
+    }
+  }
 
   if (isLoading) {
     return <div>Loading...</div>; // Render a loading state or spinner here
   }
 
-
-function getPercentageColor(tally: string) {
+  function getPercentageColor(tally: string) {
     // Extract the percentage value from the tally string, e.g., "5/10 (50%)"
     const matches = tally.match(/\((\d+\.?\d*)%\)/);
     if (matches) {
@@ -152,54 +152,54 @@ function getPercentageColor(tally: string) {
     }
     return 'orange'; // Default color if no percentage or falls between 40% and 60%
   }
-  
-    return (
-      <>
-        <Head>
-          <title>BRETON</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className={styles.banner}></div>
-        <div className={styles.navbar}>
-      <Image src="/bretpngw.png" alt="Logo" width={100} height={100} className={styles.logo} />
-      <Link href="/index" passHref></Link>
-      <ul>
-      <li><Link href="/">HOME</Link></li>
-        <li
-          onMouseEnter={() => setIsMLBDropdownVisible(true)}
-          onMouseLeave={() => setIsMLBDropdownVisible(false)}
-        >
-          MLB
-          {isMLBDropdownVisible && (
-            <div className={styles.dropdown}>
-              <Link href="/mlb"><p>Over/Under</p></Link>
-              <Link href="/mlbml"><p>ML PICKS</p></Link>
-              <Link href="/nrfi"><p>NRFI</p></Link>
-            </div>
-          )}
-        </li>
-        <li
-          onMouseEnter={() => setIsNbaDropdownVisible(true)}
-          onMouseLeave={() => setIsNbaDropdownVisible(false)}
-        >
-          NBA
-          {isNbaDropdownVisible && (
-            <div className={styles.dropdown}>
-              <Link href="/nba"><p>Over/Under</p></Link>
-              <Link href="/fullprop"><p>Player Props</p></Link>
-              <Link href="/prop"><p>Alt Player Props</p></Link>
-            </div>
-          )}
-        </li>
-        <li><Link href="/news">NEWS</Link></li>
-        <li><Link href="/ncaab">CBB</Link></li>
-      </ul>
-      <div className={styles.odds}>
-      <h4>Odds via:</h4>
-      <Image src="/dkvert.png" alt="Logo" width={70} height={60} className={styles.logor} />
+
+  return (
+    <>
+      <Head>
+        <title>BRETON</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.banner}></div>
+      <div className={styles.navbar}>
+        <Image src="/bretpngw.png" alt="Logo" width={100} height={100} className={styles.logo} />
+        <Link href="/index" passHref></Link>
+        <ul>
+          <li><Link href="/">HOME</Link></li>
+          <li
+            onMouseEnter={() => setIsMLBDropdownVisible(true)}
+            onMouseLeave={() => setIsMLBDropdownVisible(false)}
+          >
+            MLB
+            {isMLBDropdownVisible && (
+              <div className={styles.dropdown}>
+                <Link href="/mlb"><p>Over/Under</p></Link>
+                <Link href="/mlbml"><p>ML PICKS</p></Link>
+                <Link href="/nrfi"><p>NRFI</p></Link>
+              </div>
+            )}
+          </li>
+          <li
+            onMouseEnter={() => setIsNbaDropdownVisible(true)}
+            onMouseLeave={() => setIsNbaDropdownVisible(false)}
+          >
+            NBA
+            {isNbaDropdownVisible && (
+              <div className={styles.dropdown}>
+                <Link href="/nba"><p>Over/Under</p></Link>
+                <Link href="/fullprop"><p>Player Props</p></Link>
+                <Link href="/prop"><p>Alt Player Props</p></Link>
+              </div>
+            )}
+          </li>
+          <li><Link href="/news">NEWS</Link></li>
+          <li><Link href="/ncaab">CBB</Link></li>
+        </ul>
+        <div className={styles.odds}>
+          <h4>Odds via:</h4>
+          <Image src="/dkvert.png" alt="Logo" width={70} height={60} className={styles.logor} />
+        </div>
       </div>
-      </div>
-    <div className={styles.content}>
+      <div className={styles.content}>
         <div className={styles.headerContainer}>
           <h1>MLB NRFI</h1>
           <div
@@ -223,7 +223,17 @@ function getPercentageColor(tally: string) {
             )}
           </div>
         </div>
-          <div className={styles.results}>NRFI Hit Rates:<p> NRFIs: 144/233 (62%) <br /> YRFI: 40/71 (56%) </p></div>
+        <div className={styles.results}>
+          NRFI Hit Rates:
+          {nrfiResults ? (
+            <p>
+              NRFIs: {nrfiResults.NRFI.correct}/{nrfiResults.NRFI.total} ({((nrfiResults.NRFI.correct / nrfiResults.NRFI.total) * 100).toFixed(0)}%)<br />
+              YRFI: {nrfiResults.YRFI.correct}/{nrfiResults.YRFI.total} ({((nrfiResults.YRFI.correct / nrfiResults.YRFI.total) * 100).toFixed(0)}%)
+            </p>
+          ) : (
+            <p>Loading results...</p>
+          )}
+        </div>
       <table className={styles.table2} style={{ marginTop: `${tableMarginTop}px` }}>
         <thead>
           <tr>
