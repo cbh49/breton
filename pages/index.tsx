d3a07c9607c776e3f11eb8b62f45312352cce8a5
cbh49 <charlie.hill354@gmail.com>
2024-09-19 19:30:21 -0600

    Save local changes before pull - 2024-09-19 19:30:21

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // pages/index.tsx
-import styles from '../styles/home2.module.css';
+import styles from '../styles/home.module.css';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
